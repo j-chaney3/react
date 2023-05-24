cf612398044457a99dbaf290e3dd6432f8c768bf
Jame Chaney <macneilslt@gmail.com>
2023-05-24 18:00:32 -0400

    Fetch response status codes and errors

--- src/pages/Definition.js
+++ src/pages/Definition.js
@@ -6,24 +6,37 @@ import NotFound from '../components/NotFound';
 const Definition = () => {
 	const [word, setWord] = useState([]);
 	const [notFound, setNotFound] = useState(false);
+	const [error, setError] = useState(false);
 	let { search } = useParams();
 	const navigate = useNavigate();
 
 	//on first page load, fetches data from API
 	useEffect(() => {
-		fetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + search)
+		//const url = 'https://adjklfadfjakldjkslfsljfdsi32.com'
+		//const url ='https://httpstat.us/501';
+		const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/' + search;
+
+		fetch(url)
 			.then((response) => {
 				if (response.status === 404) {
 					setNotFound(true);
+				} else if (response.status === 401) {
+					navigate('/login');
+				} else if (response.status === 500) {
+					setError(true);
 				}
-
+				if (!response.ok) {
+					setError(true);
+					
+					throw new Error('Something went wrong');
+				}
 				return response.json();
 			})
 			.then((data) => {
 				setWord(data[0].meanings);
 			})
-			.catch((error) => {
-				console.error('Error fetching definition:', error);
+			.catch((e) => {
+				console.log(e.message);
 			});
 	}, []);
 
@@ -31,6 +44,16 @@ const Definition = () => {
 		return (
 			<>
 				<NotFound />
+				<Link to="/dictionary">
+					<button>Search another word</button>
+				</Link>
+			</>
+		);
+	}
+	if (error === true) {
+		return (
+			<>
+				<p>Something went wrong, try again?</p>
 				<Link to="/dictionary">
 					<button>Search another word</button>
 				</Link>
@@ -53,6 +76,9 @@ const Definition = () => {
 					})}
 				</>
 			) : null}
+			<Link to="/dictionary">
+				<button>Search another word</button>
+			</Link>
 		</>
 	);
 };
